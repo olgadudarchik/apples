6f88cce280ae17a2efc8854300ae150fdc936a0b
lidud <lidud@mail.ru>
2013-09-12 01:27:47 +0300

    Add bloom method for tree

--- src/bsu/dudarchiko/apples/Tree.java
+++ src/bsu/dudarchiko/apples/Tree.java
@@ -4,18 +4,24 @@ import java.util.Random;
 
 public class Tree {
 	private int applesCount;
+	private int flowerCount;
 
 	public Tree() {
 	}
 
 	public void grow() {
-		Random rand = new Random();
-		applesCount += rand.nextInt();
+		applesCount += flowerCount;
+		flowerCount = 0;
 	}
 
 	public void shake() {
 		Random rand = new Random();
 		applesCount += rand.nextInt(applesCount);
+	}
+	
+	public void bloom(){
+		Random rand = new Random();
+		flowerCount += rand.nextInt();
 	}
 
 }
